45f0a47034f648c110783c26775de4545d72dac6
Amiel Martin <amiel.martin@gmail.com>
2021-03-31 14:40:52 -0800

    Refactor to package
    Co-Authored-By: Michael Schoonmaker <michael.r.schoonmaker@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,4 @@
-use serde::Deserialize;
-
-const POLL_INTERVAL: std::time::Duration = std::time::Duration::from_secs(1);
-
-#[derive(Deserialize, Debug)]
-struct PersonRecord {
-    id: u32,
-    email: Option<String>,
-    name: Option<String>,
-}
-
-#[derive(Deserialize, Debug)]
-struct DebeziumPayload {
-    op: String,
-    before: Option<PersonRecord>,
-    after: Option<PersonRecord>,
-}
-
-#[derive(Deserialize, Debug)]
-struct DebeziumMessage {
-    payload: DebeziumPayload,
-}
+mod consumer;
 
 fn main() -> anyhow::Result<()> {
     println!("Starting kafka consumer");
@@ -31,35 +10,7 @@ fn main() -> anyhow::Result<()> {
         .create();
 
     match consumer {
-        Ok(consumer) => start_polling(consumer),
+        Ok(consumer) => consumer::start_polling(consumer),
         Err(error) => anyhow::bail!("OH NO {}", error),
     }
-}
-
-fn start_polling(consumer: kafka::consumer::Consumer) -> anyhow::Result<()> {
-    let mut consumer = consumer;
-
-    loop {
-        for ms in consumer.poll() {
-            for messages in ms.iter() {
-                for message in messages.messages() {
-                    match serde_json::from_slice::<DebeziumMessage>(message.value) {
-                        Ok(message) => handle_message(message)?,
-                        Err(error @ serde_json::Error { .. }) if error.is_eof() => {}
-                        Err(error) => anyhow::bail!("Could not parse message {}", error),
-                    }
-                }
-            }
-
-            if ms.is_empty() {
-                std::thread::sleep(POLL_INTERVAL);
-            }
-        }
-    }
-}
-
-fn handle_message(message: DebeziumMessage) -> anyhow::Result<()> {
-    println!("Got message {:?}", message);
-
-    Ok(())
 }
