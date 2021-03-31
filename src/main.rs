01a844e472554fb846f0aa03a6e509e2151e9db4
Amiel Martin <amiel.martin@gmail.com>
2021-03-31 12:36:00 -0800

    Hook up kafka
    Co-Authored-By: Michael Schoonmaker <michael.r.schoonmaker@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate kafka;
+
 fn main() {
     println!("Hello, world!");
+
+    let host = String::from("localhost:9094");
+    let consumer = kafka::consumer::Consumer::from_hosts(vec![host])
+        .with_topic(String::from("fernpeople.public.people"))
+        .with_fallback_offset(kafka::consumer::FetchOffset::Earliest)
+        .create();
+
+    match consumer {
+        Ok(consumer) => start_polling(consumer),
+        Err(error) => panic!("OH NO {}", error),
+    }
+}
+
+fn start_polling(consumer: kafka::consumer::Consumer) {
+    let mut consumer = consumer;
+    loop {
+        for ms in consumer.poll() {
+            for messages in ms.iter() {
+                for message in messages.messages() {
+                    println!("Got message {:?}", message);
+                }
+            }
+        }
+    }
 }
