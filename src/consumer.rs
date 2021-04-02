0f631ce3d2fcdffebdcea3d9b4bdaa4f045ceffa
Amiel Martin <amiel.martin@gmail.com>
2021-04-02 11:46:14 -0800

    Track last-consumed message with kafka

--- src/consumer.rs
+++ src/consumer.rs
@@ -6,6 +6,7 @@ const POLL_INTERVAL: std::time::Duration = std::time::Duration::from_secs(1);
 #[derive(Deserialize, Debug)]
 struct DebeziumPayload {
     op: String,
+    ts_ms: u64,
     before: Option<model::PersonRecord>,
     after: Option<model::PersonRecord>,
 }
@@ -27,7 +28,9 @@ pub fn init(options: &KafkaOptions) -> Result<kafka::consumer::Consumer, kafka::
     // TODO: Support multiple hosts
     kafka::consumer::Consumer::from_hosts(vec![options.host.clone()])
         .with_topic(options.topic.clone())
-        // .with_fallback_offset(kafka::consumer::FetchOffset::Earliest)
+        .with_fallback_offset(kafka::consumer::FetchOffset::Earliest)
+        .with_group(String::from("people-etl"))
+        .with_offset_storage(kafka::consumer::GroupOffsetStorage::Kafka)
         .create()
 }
 
@@ -49,11 +52,18 @@ pub fn start_polling(
                             Err(error) => anyhow::bail!("Could not parse message {}", error),
                         }
                     }
+                    if let Err(_) = consumer.consume_messageset(messages) {
+                        log::error!("Could not mark message set as consumed");
+                    }
                 }
 
                 if ms.is_empty() {
                     std::thread::sleep(POLL_INTERVAL);
                 }
+            }
+
+            if let Err(_) = consumer.commit_consumed() {
+                log::error!("Could not commit_consumed");
             }
         }
     })
@@ -65,18 +75,21 @@ fn convert_to_event(message: DebeziumMessage) -> model::Event {
             op,
             before: None,
             after: Some(after),
+            ..
         } if op.as_str() == "c" => model::Event::Create(after),
 
         DebeziumPayload {
             op,
             before: Some(before),
             after: Some(after),
+            ..
         } if op.as_str() == "u" => model::Event::Update(before, after),
 
         DebeziumPayload {
             op,
             before: Some(before),
             after: None,
+            ..
         } if op.as_str() == "d" => model::Event::Delete(before),
 
         DebeziumPayload { .. } => model::Event::Unknown,
